06076933fcc116c097dab7e1c0fe409ea0834252
Casey Nord <nordcw@gmail.com>
2021-04-15 16:47:07 -0700

    mult

--- calc.py
+++ calc.py
@@ -4,5 +4,9 @@ def calc(_num1, _num2):
 def subt(_num1, _num2):
     return _num1 - _num2
 
+def mult(_num1, _num2):
+    return _num1 * _num2
+
 print(calc(10, 20))
 print(subt(10, 20))
+print(mult(10, 20))
