99a22257ac5371c7283699a1470eaacbe90ea1f1
Casey Nord <nordcw@gmail.com>
2021-04-15 16:48:37 -0700

    div

--- calc.py
+++ calc.py
@@ -7,6 +7,10 @@ def subt(_num1, _num2):
 def mult(_num1, _num2):
     return _num1 * _num2
 
+def div(_num1, _num2):
+    return float(_num1) / _num2
+
 print(calc(10, 20))
 print(subt(10, 20))
 print(mult(10, 20))
+print(div(10, 20))
